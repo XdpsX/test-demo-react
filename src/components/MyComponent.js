2f05e570982554c028ed73015d3081ba99f72c11
Night <dranhata@gmail.com>
2022-11-10 17:20:15 +0700

    #20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -12,9 +12,24 @@ class MyComponent extends React.Component {
   };
   handleClick(event){
     console.log("Clicked button");
-    // console.log(event);
     console.log("My name is ", this.state.name);
+  
+    // merge State (react class)
+    this.setState({
+      name: "Hary",
+      age: Math.floor(Math.random() * 100) + 1
+    })
+    // this.setState({
+    //   age: Math.floor(Math.random() * 100) + 1
+    // })
   }
+
+  // C1: Chuyen sang arrow function de this.state.name hoat dong
+  // handleClick = (event) => {
+  //   console.log("Clicked button");
+  //   // console.log(event);
+  //   console.log("My name is ", this.state.name);
+  // }
   handleOnMouseOver(event){
     console.log(event.pageX);
   }
@@ -22,9 +37,10 @@ class MyComponent extends React.Component {
     // JSX: chi tra ve 1 phan tu parent
     return(
       <div>
-        My name is {this.state.name} and I'm from {this.state.address}
+        My name is {this.state.name} and I'm {this.state.age}
         <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
-        <button onClick={this.handleClick}>Click me</button>
+        {/* <button onClick={this.handleClick}>Click me</button> */}
+        <button onClick={(event) => this.handleClick(event)}>Click me</button>
       </div>
     )
   }
