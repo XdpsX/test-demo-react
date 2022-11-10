773782e592e6fbd8b0c004447b1aa4d5149e673b
Night <dranhata@gmail.com>
2022-11-10 21:37:48 +0700

    #22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,27 +2,24 @@
 // function component
 
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
 
-  state = {
-    name: "Eric",
-    address: "Hoi Dan IT",
-    age: 26
-  };
-  handleClick(event){
-    console.log("Clicked button");
-    console.log("My name is ", this.state.name);
   
-    // merge State (react class)
-    this.setState({
-      name: "Hary",
-      age: Math.floor(Math.random() * 100) + 1
-    })
-    // this.setState({
-    //   age: Math.floor(Math.random() * 100) + 1
-    // })
-  }
+  // handleClick(event){
+  //   console.log("Clicked button");
+  //   console.log("My name is ", this.state.name);
+  
+  //   // merge State (react class)
+  //   this.setState({
+  //     name: "Hary",
+  //     age: Math.floor(Math.random() * 100) + 1
+  //   })
+  //   // this.setState({
+  //   //   age: Math.floor(Math.random() * 100) + 1
+  //   // })
+  // }
 
   // C1: Chuyen sang arrow function de this.state.name hoat dong
   // handleClick = (event) => {
@@ -34,28 +31,14 @@ class MyComponent extends React.Component {
     console.log(event.pageX);
   }
 
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-   
-  }
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  }
+  
   render(){
     // JSX: chi tra ve 1 phan tu parent
     return(
       <div>
         {/* <button onMouseOver={this.handleOnMouseOver}>Hover me</button> */}
         {/* <button onClick={this.handleClick}>Click me</button> */}
-        My name is {this.state.name} and I'm {this.state.age}
-        <button onClick={(event) => this.handleClick(event)}>Click me</button>
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <input type="text" onChange={(event) => this.handleOnChangeInput(event)}/>
-          <button>Submit</button>
-        </form>
+        <UserInfor></UserInfor>
       </div>
     )
   }
